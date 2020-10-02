02dd54c59963bfca9d17dbe56288ac8b1067d57a
happymink <71982555+happymink@users.noreply.github.com>
2020-10-02 17:39:25 +0900

    네이버 뉴스 기사 웹 크롤링 - 타이틀 수집 - url 수집 - 언론사 수집

--- sparta/venv/week1_homework.py
+++ sparta/venv/week1_homework.py
@@ -8,7 +8,9 @@ driver.get("https://search.daum.net/search?w=img&nil_search=btn&DA=NTB&enc=utf8&
 time.sleep(5) # 5초 동안 페이지 로딩 기다리기
 
 req = driver.page_source
-
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
 soup = BeautifulSoup(req, 'html.parser')
 
 thumbnails = soup.select('#imgList > div > a > img')
