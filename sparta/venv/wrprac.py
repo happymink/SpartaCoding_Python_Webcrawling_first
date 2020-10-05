3559650a2bca0d353201a24829b45edab6ad56e3
happymink <71982555+happymink@users.noreply.github.com>
2020-10-05 20:48:37 +0900

    1005 파이썬 3일차 텍스트 파일 만들기 카카오톡에서 대화내용 받아와서 클라우드 만들기 구름 이미지를 다운 받아서 클라우드 틀 맞춰서 다시만듬

--- sparta/venv/wrprac.py
+++ sparta/venv/wrprac.py
@@ -1,13 +1,24 @@
 from wordcloud import WordCloud
-
+from PIL import Image
+import numpy as np
 text = ''
 with open("kakaotalk.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
-    for line in lines:
-        text += line
+    for line in lines[2:]:
+        if '] [' in line:
+            text += line.split('] ')[2].replace('ㅋ','').replace('ㅎ','').replace('ㅠ','').replace('ㅜ','').replace('이모티콘\n','').replace('사진\n','').replace('저는','').replace('저도','')
+
+
 print(text)
 
 
 wc = WordCloud(font_path='C:/WINDOWS/Fonts/NanumBarunGothic.ttf', background_color="white", width=600, height=400)
 wc.generate(text)
 wc.to_file("result.png")
+
+
+
+mask = np.array(Image.open('cloud.png'))
+wc = WordCloud(font_path='C:/WINDOWS/Fonts/NanumBarunGothic.ttf', background_color="white", mask=mask)
+wc.generate(text)
+wc.to_file("result_masked.png")
