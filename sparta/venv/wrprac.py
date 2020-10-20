ae436cc59735ec791909ca736184bfd460137e12
happymink <71982555+happymink@users.noreply.github.com>
2020-10-20 23:37:19 +0900

    1
    1

--- sparta/venv/wrprac.py
+++ sparta/venv/wrprac.py
@@ -2,7 +2,7 @@ from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
 text = ''
-with open("kakaotalk.txt", "r", encoding="utf-8") as f:
+with open("1020talk.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines[2:]:
         if '] [' in line:
@@ -12,13 +12,13 @@ with open("kakaotalk.txt", "r", encoding="utf-8") as f:
 print(text)
 
 
-wc = WordCloud(font_path='C:/WINDOWS/Fonts/NanumBarunGothic.ttf', background_color="white", width=600, height=400)
+wc = WordCloud(font_path='C:/WINDOWS/Fonts/HOONSAEMAULUNDONGR.ttf', background_color="white", width=600, height=400)
 wc.generate(text)
 wc.to_file("result.png")
 
 
 
 mask = np.array(Image.open('cloud.png'))
-wc = WordCloud(font_path='C:/WINDOWS/Fonts/NanumBarunGothic.ttf', background_color="white", mask=mask)
+wc = WordCloud(font_path='C:/WINDOWS/Fonts/HOONSAEMAULUNDONGR.ttf', background_color="white", mask=mask)
 wc.generate(text)
-wc.to_file("result_masked.png")
+wc.to_file("result_masked2.png")
